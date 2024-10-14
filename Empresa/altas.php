251b0d19ca8a43bd2482eec8e0228885774b72da
PedroS <pedro.sandoval.martinez@ces-vegamedia.es>
2024-10-14 12:50:46 +0200

    altas bien por fin

--- Empresa/altas.php
+++ Empresa/altas.php
@@ -1,26 +1,20 @@
 <?php
 //Estableciendo la conexión
-include ("conexion.php");
+include("conexion.php");
 //recogida de datos
-$cod=$_GET['codigo'];
-$prod=$_GET['producto'];
-$detail=$_GET['detalle'];
-$precio=$_GET['precio'];
-$dto=$_GET['descuento'];
-$imag="images/". $cod. ".jpg";
-
-//creamos la sentencia SQL
+$cod = $_POST['codigo'];
+$pro = $_POST['producto'];
+$det = $_POST['detalle'];
+$pre = $_POST['precio'];
+$des = $_POST['descuento'];
+$ima = $cod . ".jpg";
 $consulta = "INSERT INTO productos (codigo,producto,detalle,precio,descuento,imagen) 
-VALUES ('$cod','$prod','$detail','$precio','$dto','$imag')";
-//ejecutamos la sentencia SQL, atributos conn en conexion.php 
-mysqli_query($conn,$consulta);
+VALUES ('$cod','$pro','$det','$pre','$des','$ima')";
+//ejecutamos la sentencia SQL
+mysqli_query($conn, $consulta);
 //copiamos la imagen que nos ha llegado a su carpeta.
 echo mysqli_error($conn);
-$ruta=$imag;
-//copiamos imagen a la carpeta images
-copy($ruta,"images/".$cod.".jpg");
-
+$ruta = $ima;
+COPY($_FILES["imagen"]["tmp_name"], $ruta);
 //redireccionamos a la web listados (este fichero lo debeis de crar vosotros)
-echo "Producto añadido exitosamente";
-header ("LOCATION:listado.php");
-?>
+header("LOCATION:listado.php");
--- Empresa/form/altasFormulario.php
+++ Empresa/form/altasFormulario.php
@@ -6,7 +6,7 @@
 
 <body>
     <h1 class="altasTitle"> Altas de Productos </h1>
-    <form action="../altas.php" method="get" enctype="multipart/form-data">
+    <form action="../altas.php" method="post" enctype="multipart/form-data">
         <div class="primeraColum">
             <label for="codigo">Codigo</label>
             <input type="text" name="codigo" required />
